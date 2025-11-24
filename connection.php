f28cb0b48c41a4933b9e6256989e187e3350f49a
Aidan-Joe <aidanjoe88@gmail.com>
2025-11-25 06:24:13 +0700

    .

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@ $DB_HOST = getenv('DB_HOST') ?: 'mysql';
 $DB_USER = getenv('DB_USER') ?: 'root';
 $DB_PASS = getenv('DB_PASSWORD') ?: 'password';
 $DB_NAME = getenv('DB_NAME') ?: 'db_museum';
-$DB_PORT = getenv('DB_PORT') ?: 3309;
+$DB_PORT = getenv('DB_PORT') ?: 3306;
 
 $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
 
